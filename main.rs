c2773c4b60a79f4d536d9ccfa3449fad0d446cc5
alaotach <aryanmi2001@gmail.com>
2026-02-21 21:33:46 +0530

    created default grid

--- main.rs
+++ main.rs
@@ -1,3 +1,33 @@
+const W: usize = 10;
+const H: usize = 15;
+
+struct Board {
+    cells: [[u8; W]; H],
+}
+
+impl Board {
+    fn new() -> Self {
+        Board {
+            cells: [[0; W]; H],
+        }
+    }
+
+    fn render(&self) {
+        for y in 0..H {
+            for x in 0..W {
+                if self.cells[y][x] == 0 {
+                    print!(". ");
+                }
+                else {
+                    print!("██");
+                }
+            }
+            println!();
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let board = Board::new();
+    board.render();
 }
